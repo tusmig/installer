b3f6f88d17cb8befe7d052134e09d91ff292092b
jd <jd>
2020-03-03 14:27:22 +0000

    bugFix

--- Folder_create/main.tf
+++ Folder_create/main.tf
@@ -3,9 +3,11 @@
 ##      Created 3/3/20 by ShowMe team. For Folder_create for Folder_create
 ##
 #####################################################################
-
+data "vsphere_datacenter" "dc" {
+  name = "${var.datacenter}"
+}
 resource "vsphere_folder" "folder" {
   path          = "${var.path}"
   type          = "vm"
-  datacenter_id = "${var.datacenter_id}"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
--- Folder_create/variables.tf
+++ Folder_create/variables.tf
@@ -3,14 +3,14 @@
 ##      Created 3/3/20 by ShowMe team. For Folder_create for Folder_create
 ##
 #####################################################################
-variable "datacenter_id" {
+variable "datacenter" {
   type = "string"
   description = "Name of Datacenter for ShowMe"
 }
 
 variable "path" {
   type = "string"
-  description = "Folder Path for ShowMe"
+  description = "Folder Path for ShowMe; usage: /folder/subfolder"
 }
 
 
