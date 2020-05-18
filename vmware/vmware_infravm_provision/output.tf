d0796550f72d70a741f0b1ded8734f0733a4b4fe
Mano Iglesias <manolito.iglesias-ares@nl.ibm.com>
2020-05-18 18:50:15 +0000

    test

--- vmware/vmware_infravm_provision/output.tf
+++ vmware/vmware_infravm_provision/output.tf
@@ -4,5 +4,5 @@ output "dependsOn" {
 }
 
 output "vm_mgmt_network_ip" {
-  value = "${vsphere_virtual_machine.vm.var.ip}"
+  value = "${vsphere_virtual_machine.vm2disk.default_ip_address}"
 }            
